4dc3d04ffe3b56f655b0fb7272b417b890123712
sphinx <oandreeva88@mail.ru>
2021-08-17 19:44:43 +0300

    context was added, static was set in products, fix error: application products was added in settings

--- products/views.py
+++ products/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+import json
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+        'h1_heading': 'GeekShop Store',
+        'description': 'Новые образы и лучшие бренды на GeekShop Store.\n'
+                       'Бесплатная доставка по всему миру! Аутлет: до -70% Собственный бренд. -20% новым покупателям.',
+        'button_text_to_start_shopping': 'Начать покупки'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    with open('products/fixtures/products.json', 'r') as fp:
+        items = json.load(fp)
+    context = {
+        'items': items,
+        'title': 'GeekShop - Каталог',
+        'groups': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки']
+    }
+    return render(request, 'products/products.html', context)
