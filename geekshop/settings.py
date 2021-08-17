ba006978727277d40767df0d9ac0078f9a205c4b
sphinx <oandreeva88@mail.ru>
2021-08-17 22:13:32 +0300

    urls work ok

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'products' / 'templates'],
+        'DIRS': [],  # BASE_DIR / 'products' / 'templates'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
